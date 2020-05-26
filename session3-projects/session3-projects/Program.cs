5867b0b05f2d46a9c4ecb41f33f6f0247619ce60
reza <shalchian.r@gmail.com>
2020-05-26 13:52:10 -0400

    predicate

--- session3-projects/session3-projects/Program.cs
+++ session3-projects/session3-projects/Program.cs
@@ -11,7 +11,62 @@ namespace session3_projects
         public delegate void Display(int n);
         public delegate int Operation(int x, int y);
 
+        public delegate void PrintDelegate(int value);
+
         static void Main(string[] args)
+        {
+            //DelegateExample();
+            //AnnonymousType();
+
+            //Predicate
+            Predicate<string> isUppper = IsUpperCase;
+            bool result = isUppper("HHHHH!");
+            Console.WriteLine(result);
+
+            Predicate<string> isUpper2 = delegate (string str)
+            {
+                return str.Equals(str.ToUpper());
+            };
+            result = isUpper2("value test");
+
+            Predicate<int> isOdd = IsOdd;
+            result = isOdd(15);
+        }
+      
+        static bool IsUpperCase(string str)
+        {
+            return str.Equals(str.ToUpper());
+        }
+
+        static bool IsOdd(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        public static void AnnonymousType()
+        {
+            //Annonynmous method
+            var person = new { id = 1, name = "A" };
+
+            int i = 10;
+            PrintDelegate print = delegate (int value)
+            {
+                //they have access to the outer variables
+                value += i;
+                Console.WriteLine("value is {0}", value);
+            };
+
+            PrintDelegate dddd = TEMP;
+
+            print(100);
+        }
+
+        public static void TEMP(int val)
+        {
+            Console.WriteLine("value is {0}", val);
+        }
+
+        public static void DelegateExample()
         {
             Display display = new Display(PrettyPrint);
             display += Print;
@@ -22,7 +77,6 @@ namespace session3_projects
 
             DelegateExample2 example2 = new DelegateExample2();
             example2.UseDelegate();
-
         }
 
         private static void Calculate(Display display, Operation operation)
