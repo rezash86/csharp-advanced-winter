5b7a0479283eed16416054633aa4ddeb1356b2a9
reza <shalchian.r@gmail.com>
2020-06-01 13:55:46 -0400

    adding more code

--- session7-projects/session7-projects/RespondToChanges.xaml.cs
+++ session7-projects/session7-projects/RespondToChanges.xaml.cs
@@ -72,19 +72,19 @@ namespace session7_projects
                 if(this.name != value)
                 {
                     this.name = value;
-                    //this.NotifyPropertyChanged("Name");
+                    this.NotifyPropertyChanged("Name");
                 }
             }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        //public void NotifyPropertyChanged(string propName)
-        //{
-        //    if(this.PropertyChanged != null)
-        //    {
-        //        this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
-        //    }
-        //}
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
     }
 }
