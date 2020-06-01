7022dab9e6ca5a9c5e5e1de48306af5babfe6360
reza <shalchian.r@gmail.com>
2020-06-01 17:52:17 -0400

    part 2

---
+++ session7-projects/session7-projects/DatePickerExample.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace session7_projects
+{
+    /// <summary>
+    /// Interaction logic for DatePickerExample.xaml
+    /// </summary>
+    public partial class DatePickerExample : Window
+    {
+        public DatePickerExample()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtName.Text;
+            DateTime birthDate = dtPickerSample.SelectedDate.Value;
+            var gender = (cmbGender.SelectedItem as ComboBoxItem).Content;
+
+            //Pass these values into a method
+            MessageBox.Show($"{name} was born in {birthDate} and => {gender}");
+        }
+    }
+}
---
+++ session7-projects/session7-projects/service/EmployeeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace session7_projects.service
+{
+    class EmployeeService
+    {
+    }
+}
